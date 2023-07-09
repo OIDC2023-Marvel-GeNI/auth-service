3dd1fe74f1de4fc54eb42fbfd89c0bab5c73d7aa
bakhuiwon326 <79272597+bakhuiwon326@users.noreply.github.com>
2023-07-09 20:22:03 +0900

    set main

--- src/test/java/marvel/geni/authservice/AuthServiceApplicationTests.java
+++ src/test/java/marvel/geni/authservice/AuthServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package marvel.geni.auth;
+package marvel.geni.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthApplicationTests {
+class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
